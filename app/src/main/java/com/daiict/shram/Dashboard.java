06636003f2516f6ea05446c7b08580a1cb59c01a
Rishabh Singh <pingrishabh@gmail.com>
2017-11-11 17:50:39 +0530

    Second Commit
    Setting Up The Project For All Contributors

---
+++ app/src/main/java/com/daiict/shram/Dashboard.java
@@ -0,0 +1,13 @@
+package com.daiict.shram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
